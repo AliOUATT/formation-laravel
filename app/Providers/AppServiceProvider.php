15eb12a43716ed6ebe2bf9dd4a8edc217f83fc39
ali.ouattara <ali.ouattara1911@gmail.com>
2021-04-02 12:07:55 +0000

    url & dbseeders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        Url::forceScheme('https');
+        URL::forceScheme('https');
     }
 }
